2586896d34a4c55470cacc124577803fac525964
Nadia <nadi.fatah@gmail.com>
2022-01-20 08:11:54 -0500

    Start to build Appointment component

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "components/Appointments/styles.scss";
+
+
+export default function Appointment(props) {
+  return (
+<article className="appointment"></article>
+  );
+}
