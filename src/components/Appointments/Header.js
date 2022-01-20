da719dd55d12c79df20e9d9f35bea54d5143a8d5
Nadia <nadi.fatah@gmail.com>
2022-01-20 10:27:55 -0500

    Fix Header error

--- src/components/Appointments/Header.js
+++ src/components/Appointments/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-export default function Header(time) {
+export default function Header({time}) {
   return (
 <header className="appointment__time">
   <h4 className="text--semi-bold">{time}</h4>
