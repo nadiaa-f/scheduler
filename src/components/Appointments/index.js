e1c3590ba82f1449eebb370a4c700209169c894c
Nadia <nadi.fatah@gmail.com>
2022-01-20 21:35:17 -0500

    Display appointments

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "components/Appointments/styles.scss";
-
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  return (
-<article className="appointment"></article>
+  return ( props.interview ? 
+    <article className="appointment">
+      <Header time={props.time} />
+      <Show interview={props.interview} student={props.interview.student} interviewer={props.interview.interviewer}/>
+    </article>
+  :
+    <article className="appointment">
+    <Header time={props.time}/>
+    <Empty />
+    </article>
   );
-}
+  }
