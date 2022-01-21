0cfc488e435cd98c202fa4b2e1f39555256262c3
Nadia <nadi.fatah@gmail.com>
2022-01-20 21:54:00 -0500

    Add views to in Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,48 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
+import "components/Appointments";
 import DayList from "./DayList";
+import Appointment from "./Appointments";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 const days = [
   {
@@ -39,7 +80,8 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      {appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
