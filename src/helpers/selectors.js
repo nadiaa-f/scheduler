27ed2b4236a00720fcaddcb919173110db90a6f3
Nadia <nadi.fatah@gmail.com>
2022-01-21 07:25:33 -0500

    Begin to transform data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,8 @@
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+
+  //1find object in state.says array that === the provided day
+  //2with that provided days appointment array
+  //itterate through and compare id and id states.appointment return value
+  //3!appointment on that provide day return = []
+}
