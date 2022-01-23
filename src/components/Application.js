c3b37637e20a9effb0ad45dddf930831e4213238
Nadia <nadi.fatah@gmail.com>
2022-01-23 14:40:00 -0500

    Use promise all function

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,62 +5,28 @@ import "components/Appointments";
 import DayList from "./DayList";
 import Appointment from "./Appointments";
 import axios from "axios";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
-  
-
+  const setDay = day => setState({ ...state, day });
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: []
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState((prev => ({ ...prev, days })));
+  let dailyAppointments = getAppointmentsForDay(state, state.day)
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-    .then(res => setDays([...res.data]))
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments")
+    ])
+    .then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+     })
   }, []);
+     
   return (
     <main className="layout">
       <section className="sidebar">
@@ -78,7 +44,7 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-      {appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)}
+      {dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
