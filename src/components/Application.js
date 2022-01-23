e7318e8ed6751c7f861272e8076e64ecfd4fde20
Nadia <nadi.fatah@gmail.com>
2022-01-23 13:53:31 -0500

    Combine all of the state in Application into a single object called state

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,7 +46,17 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [days, setDays] = useState("");
+  
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: []
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState((prev => ({ ...prev, days })));
+
   useEffect(() => {
     axios.get("http://localhost:8001/api/days")
     .then(res => setDays([...res.data]))
@@ -60,7 +70,7 @@ export default function Application(props) {
   alt="Interview Scheduler"
 />
 <hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"><DayList days={days} value={day} onChange={setDay} /></nav>
+<nav className="sidebar__menu"><DayList days={state.days} value={state.day} onChange={setDay} /></nav>
 <img
   className="sidebar__lhl sidebar--centered"
   src="images/lhl.png"
