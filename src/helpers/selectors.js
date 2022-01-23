61aa7c4579b5aa5683314af8cc8c0102022da105
Nadia <nadi.fatah@gmail.com>
2022-01-23 10:14:21 -0500

    Transform data with selectors complete, all tests pass

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,13 @@
 export function getAppointmentsForDay(state, day) {
-  //... returns an array of appointments for that day
-
-  //1find object in state.says array that === the provided day
-  //2with that provided days appointment array
-  //itterate through and compare id and id states.appointment return value
-  //3!appointment on that provide day return = []
+  let allAppointmentsArray = []; 
+  let appointments = []
+  for (let days of state.days) {
+    if (days.name === day) {
+      allAppointmentsArray = days.appointments
+    }
+  }
+  for (let a = 0; a < allAppointmentsArray.length; a++) {
+    appointments.push(state.appointments[allAppointmentsArray[a]])
+  }
+  return appointments;
 }
