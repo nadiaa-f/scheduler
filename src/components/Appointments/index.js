482618b8a861d49a00301ad1b70e724f1be45902
Nadia <nadi.fatah@gmail.com>
2022-01-23 19:33:03 -0500

    Use the useVisualMode hook to determine the mode and then conditionally render the matching component

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -3,17 +3,29 @@ import "components/Appointments/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  return ( props.interview ? 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW"; 
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  return (
     <article className="appointment">
       <Header time={props.time} />
-      <Show interview={props.interview} student={props.interview.student} interviewer={props.interview.interviewer}/>
-    </article>
-  :
-    <article className="appointment">
-    <Header time={props.time}/>
-    <Empty />
-    </article>
-  );
-  }
+      {mode === EMPTY && <Empty onAdd={() => transition("CREATE")} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        )}
+        {mode === CREATE && <Form interviewers = {[]} onCancel= {() => back()}/>}
+      </article>
+   );
+ } 
