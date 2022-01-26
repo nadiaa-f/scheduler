0a32ed765ec1c52451b372ce7c013d720c2af765
Nadia <nadi.fatah@gmail.com>
2022-01-26 15:32:47 -0500

    Fix deleting error, now working

--- src/components/Appointments/Confirm.js
+++ src/components/Appointments/Confirm.js
@@ -6,8 +6,8 @@ export default function Confirm({message, onConfirm, onCancel}) {
     <main className="appointment__card appointment__card--confirm">
   <h1 className="text--semi-bold">Delete the appointment?</h1>
   <section className="appointment__actions">
-    <Button danger>Cancel</Button>
-    <Button danger>Confirm</Button>
+    <Button onClick={onCancel} danger>Cancel</Button>
+    <Button onClick={onConfirm} danger>Confirm</Button>
   </section>
 </main>
   );
